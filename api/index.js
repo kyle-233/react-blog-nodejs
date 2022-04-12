892c2f0527f83487f636a8a20dbe96328917ed9a
kyle-233 <kyle07142113@163.com>
2022-04-12 09:56:29 +0800

    feat(upload): 上传图片接口

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer")
 
 dotenv.config();
 app.use(express.json())
@@ -17,6 +18,20 @@ mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")
 //   useUnifiedTopology: true,
 //   useCreateIndex: true,
 // });
+
+const storage = multer.diskStorage({
+  destination:(req, file, cb) => {
+    cb(null, "iamges")
+  }, 
+  filename:(req, file, cb) => {
+    cb(null, req.body.name);
+  }
+})
+
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded")
+})
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
